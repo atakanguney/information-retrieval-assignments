dd588deb4ff7325f0254587d526aa5edcc4e9130
atakanguney <atakanguney58@hotmail.com>
2019-03-16 22:03:13 +0300

    refactor preprocessing
    * change implementation of preprocessing into object oriented manner
    * print results requested in report

--- assignment-1/workspace/Code/preprocessing.py
+++ assignment-1/workspace/Code/preprocessing.py
@@ -1,99 +1,121 @@
-import concurrent.futures
 import json
 import os
 import re
-from functools import reduce
+from collections import Counter
 
 
-def get_records(raw_text):
-    # TODO: Write doc-string
-    p = re.compile(r"<REUTERS(.*?)</REUTERS>", re.DOTALL)
-    for match in p.finditer(raw_text):
-        yield match.group(0)
-
-
-def get_id(record):
-    # TODO: Write doc-string
-    p = re.compile(r'NEWID="(\d*?)"', re.DOTALL)
-    match = p.search(record)
-    if not match:
-        print(record)
-        raise ValueError("ID cannot be found")
-    return match.group(1)
-
-
-def get_tag_from_text(text, tag):
-    """Get title between <'tag'> tags
-
-    Parameters
-    ==========
-    text: str
-        Text to extract 'tag'
-
-    Returns
-    =======
-        str:
-            Matched 'tag'
+class Reader(object):
+    """Reader class to read data
     """
-    reg_pat = r"<{}>(.*?)</{}>".format(tag, tag)
-    p = re.compile(reg_pat, re.DOTALL)
-    match = p.search(text)
-    if match:
-        res = match.group(1)
-    else:
-        res = ""
 
-    return res
+    @staticmethod
+    def get_records(raw_text):
+        """Get records from raw text
 
+        Parameters
+        ==========
+        raw_text: str
+            Raw text data read from file
 
-def extract_id_document(record):
-    """Get id, title and body
+        Yields
+        ======
+            str:
+                Record
+        """
+        p = re.compile(r"<REUTERS(.*?)</REUTERS>", re.DOTALL)
+        for match in p.finditer(raw_text):
+            yield match.group(0)
 
-    Parameters
-    ==========
-    record: str
-        Text to extract
+    @staticmethod
+    def get_id(record):
+        """Get id of a record
 
-    Returns
-    =======
-        str:
-            id, title and body
-    """
-    id_ = get_id(record)
-    text = get_tag_from_text(record, "TEXT").strip()
-    title = get_tag_from_text(text, "TITLE").strip()
-    body = get_tag_from_text(text, "BODY").strip()
+        Parameters
+        ==========
+        record: str
 
-    return int(id_), (title + " " + body).strip()
+        Returns
+        =======
+            int:
+                id of the record
+        """
+        p = re.compile(r'NEWID="(\d*?)"')
+        match = p.search(record)
+        if not match:
+            raise ValueError("ID cannot be found in record: {}".format(record))
+        return int(match.group(1))
 
+    @staticmethod
+    def get_tag_from_text(text, tag):
+        """Get title between <'tag'> tags
 
-def read_input(path):
-    """Read whole file
+        Parameters
+        ==========
+        text: str
+            Text to extract 'tag'
 
-    Parameters
-    ==========
-    path: str
-        Path to file
+        Returns
+        =======
+            str:
+                Matched 'tag'
+        """
+        reg_pat = r"<{}>(.*?)</{}>".format(tag, tag)
+        p = re.compile(reg_pat, re.DOTALL)
+        match = p.search(text)
+        if match:
+            res = match.group(1)
+        else:
+            res = ""
 
-    Returns
-    =======
-        str:
-            Raw Text
-    """
-    with open(path, "rb") as f:
-        return f.read().decode("latin-1")
+        return res
 
+    @staticmethod
+    def read_input(path):
+        """Read whole file
 
-def read_punctuations(path):
-    # TODO: Write doc-string    
-    with open(path, "r") as f:
-        return f.read().strip().split()
+        Parameters
+        ==========
+        path: str
+            Path to file
 
+        Returns
+        =======
+            str:
+                Raw Text
+        """
+        with open(path, "rb") as f:
+            return f.read().decode("latin-1")
 
-def read_stopwords(path):
-    # TODO: Write doc-string
-    with open(path, "r") as f:
-        return f.read().strip().split()
+    @classmethod
+    def extract_id_document(cls, record):
+        """Get id, title and body
+
+        Parameters
+        ==========
+        record: str
+            Text to extract
+
+        Returns
+        =======
+            str:
+                id, title and body
+        """
+        id_ = cls.get_id(record)
+        text = cls.get_tag_from_text(record, "TEXT").strip()
+        title = cls.get_tag_from_text(text, "TITLE").strip()
+        body = cls.get_tag_from_text(text, "BODY").strip()
+
+        return id_, (title + " " + body).strip()
+
+    @classmethod
+    def get_all_id_text(cls, paths):
+        id_text = dict([
+            cls.extract_id_document(record)
+            for path in paths
+            for record in cls.get_records(cls.read_input(path))
+        ])
+
+        return id_text
 
 
 class Tokenizer(object):
@@ -113,9 +135,70 @@ class Tokenizer(object):
     stopwords_path: str
         Path to stopwords list
     """
+    @staticmethod
+    def read_punctuations(path):
+        # TODO: Write doc-string    
+        with open(path, "r") as f:
+            return f.read().strip().split()
+
+    @staticmethod
+    def read_stopwords(path):
+        # TODO: Write doc-string
+        with open(path, "r") as f:
+            return f.read().strip().split()
+
+    @staticmethod
+    def get_number_of_tokens(id_tokens):
+        # TODO: Write doc-string
+        all_tokens = []
+        for tokens in id_tokens.values():
+            all_tokens.extend(tokens)
+        return len(all_tokens)
+
+    @staticmethod
+    def get_number_of_terms_and_top_20(id_tokens):
+        # TODO: Write doc-string
+        all_terms = []
+        for tokens in id_tokens.values():
+            all_terms.extend(tokens)
+        top_20_terms = Counter(all_terms).most_common(20)
+        all_terms = set(all_terms)
+        return len(all_terms), top_20_terms
+
     def __init__(self, punctuations_path, stopwords_path):
-        self.punctuations = read_punctuations(punctuations_path)
-        self.stopwords = read_stopwords(stopwords_path)
+        self.punctuations = self.read_punctuations(punctuations_path)
+        self.stopwords = self.read_stopwords(stopwords_path)
+
+    def replace_punctuations(self, text):
+        # TODO: Write doc-string
+        for punc in self.punctuations:
+            text = text.replace(punc, " ")
+        return text
+
+    def remove_stopwords(self, words):
+        # TODO: Write doc-string
+        return [
+            word for word in words
+            if word not in self.stopwords
+        ]
+
+    def split_tokens(self, text):
+        # TODO: Write doc-string
+        return text.strip().split()
+
+    def replace_punctuations_and_split_tokens(self, text):
+        # TODO: Write doc-string
+        # Replace puntuations with empty space
+        text = self.replace_punctuations(text)
+
+        # Split by space
+        tokens = self.split_tokens(text)
+
+        return tokens
+
+    def casefolding(self, tokens):
+        # TODO: Write doc-string
+        return [word.casefold() for word in tokens]
 
     def tokenize(self, text):
         """Tokenize given text
@@ -125,52 +208,31 @@ class Tokenizer(object):
         text: str
             Text to tokenize
         """
-        # Replace puntuations with empty space
-        for punc in self.punctuations:
-            text = text.replace(punc, " ")
-        words = text.strip().split()
+        # Replace punctuations and split tokens
+        tokens = self.replace_punctuations_and_split_tokens(text)
 
         # Case-Folding
-        words = [word.casefold() for word in words]
+        tokens = self.casefolding(tokens)
 
-        # Remove stop-words
-        return [
-            word for word in words
-            if word not in self.stopwords
-        ]
+        # Remove stopwords
+        tokens = self.remove_stopwords(tokens)
 
-
-def merge_indices(indices):
-    # TODO: Write doc-string
-    output = {}
-    for index in indices:
-        for key, value in index.items():
-            output.setdefault(key, set({})).update(value)
-
-    return output
+        return tokens
 
 
 class Indexer(object):
     # TODO: Write doc-string
-    def __init__(self, tokenizer):
-        self.tokenizer = tokenizer
-
-    def extract_tokens(self, path):
+    @staticmethod
+    def merge_indices(indices):
         # TODO: Write doc-string
-        raw_text = read_input(path)
-        records = get_records(raw_text)
-        news_texts = dict([
-            extract_id_document(record)
-            for record in records
-        ])
-        return {
-            id_: {
-                *self.tokenizer.tokenize(news_text)
-            }
-            for id_, news_text in news_texts.items()
-        }
+        output = {}
+        for index in indices:
+            for key, value in index.items():
+                output.setdefault(key, set({})).update(value)
 
-    def construct_bigram_index(self, word):
+        return output
+
+    def extract_bigram_index(self, word):
         # TODO: Write doc-string
         bigram_index = {}
         word_ = "$" + word + "$"
@@ -178,59 +240,44 @@ class Indexer(object):
             bigram_index.setdefault(word_[i:i+2], set()).add(word)
         return bigram_index
 
-    def construct_indices_from_id_words(self, id_words):
+    def construct_indices_from_id_terms(self, id_terms):
         # TODO: Write doc-string
         inverted_index = {}
         bigram_indices = []
         
-        for index, words in id_words.items():
-            for word in words:
-                if word:
-                    inverted_index.setdefault(word, set()).add(index)
-                    bigram_indices.append(self.construct_bigram_index(word))
-        return inverted_index, merge_indices(bigram_indices)
+        for index, terms in id_terms.items():
+            for term in terms:
+                if term:
+                    inverted_index.setdefault(term, set()).add(index)
+                    bigram_indices.append(self.extract_bigram_index(term))
 
-    def helper(self, path):
+        return inverted_index, self.merge_indices(bigram_indices)
+
+    def get_id_terms(self, id_tokens):
         # TODO: Write doc-string
-        id_words = self.extract_tokens(path)
-        return self.construct_indices_from_id_words(id_words)
+        return {
+            id_: set(tokens)
+            for id_, tokens in id_tokens.items()
+        }
 
-    def construct_all_indices(self, paths, parallelism=None):
+    def construct_indices(self, id_tokens):
         # TODO: Write doc-string
-        if not parallelism:
-            parallelism = os.cpu_count()
-        
-        with concurrent.futures.ProcessPoolExecutor(max_workers=parallelism) as executor:
-            inverted_bigram = list(executor.map(
-                self.helper,
-                paths
-            ))
+        id_terms = self.get_id_terms(id_tokens)
+        return self.construct_indices_from_id_terms(id_terms)
 
-        inverted_indices = [
-            tuple_[0]
-            for tuple_ in inverted_bigram
-        ]
+    @staticmethod
+    def save_index(path, index):
+        # TODO: Write doc-string
+        with open(path, "w") as f:
+            f.write(json.dumps(index))
 
-        bigram_indices = [
-            tuple_[1]
-            for tuple_ in inverted_bigram
-        ]
-
-        return merge_indices(inverted_indices), merge_indices(bigram_indices)
-
-
-def save_index(path, index):
-    # TODO: Write doc-string
-    with open(path, "w") as f:
-        f.write(json.dumps(index))
-
-
-def convert_set_to_list(dict_):
-    # TODO: Write doc-string
-    return {
-        key: list(val)
-        for key, val in dict_.items()
-    }
+    @staticmethod
+    def convert_set_to_list(dict_):
+        # TODO: Write doc-string
+        return {
+            key: list(val)
+            for key, val in dict_.items()
+        }
 
 
 if __name__ == "__main__":
@@ -248,8 +295,45 @@ if __name__ == "__main__":
         raise ValueError("Folder not found: {}".format(base_folder))
     file_paths = [os.path.join(base_folder, "reut2-{:03d}.sgm".format(i)) for i in range(22)]
 
+    reader = Reader()
+    id_text = reader.get_all_id_text(file_paths)
+
     tokenizer = Tokenizer(punctuations_path, stopwords_path)
-    indexer = Indexer(tokenizer)
-    inverted_index_set, bigrams_set = indexer.construct_all_indices(file_paths)
-    save_index(inverted_index_path, convert_set_to_list(inverted_index_set))
-    save_index(bigrams_path, convert_set_to_list(bigrams_set))
+    # Replace punctuations and split by space
+    id_tokens = {
+        id_: tokenizer.replace_punctuations_and_split_tokens(text)
+        for id_, text in id_text.items()
+    }
+    number_of_terms, top_20_terms = tokenizer.get_number_of_terms_and_top_20(id_tokens)
+    print("Number of terms before stopword removal and casefolding: {}".format(number_of_terms))
+    print("Top 20 terms before stopword removal and casefolding:")
+    for term, freq in top_20_terms:
+        print("Term: {}, Frequency: {}".format(term, freq))
+
+    # Case folding
+    id_tokens = {
+        id_: tokenizer.casefolding(tokens)
+        for id_, tokens in id_tokens.items()
+    }
+    number_of_tokens = tokenizer.get_number_of_tokens(id_tokens)
+    print("Number of tokens before stopword removal: {}".format(number_of_tokens))
+    # Stopword removal
+    id_tokens = {
+        id_: tokenizer.remove_stopwords(tokens)
+        for id_, tokens in id_tokens.items()
+    }
+    number_of_tokens = tokenizer.get_number_of_tokens(id_tokens)
+    print("Number of tokens after stopword removal: {}".format(number_of_tokens))
+    number_of_terms, top_20_terms = tokenizer.get_number_of_terms_and_top_20(id_tokens)
+    print("Number of terms after stopword removal and casefolding: {}".format(number_of_terms))
+    print("Top 20 terms after stopword removal and casefolding:")
+    for term, freq in top_20_terms:
+        print("Term: {}, Frequency: {}".format(term, freq))
+
+
+    indexer = Indexer()
+    inverted_index, bigram_index = indexer.construct_indices(id_tokens)
+    inverted_index = Indexer.convert_set_to_list(inverted_index)
+    bigram_index = Indexer.convert_set_to_list(bigram_index)
+    Indexer.save_index(inverted_index_path, inverted_index)
+    Indexer.save_index(bigrams_path, bigram_index)
